0a074e569c00c4eddad3ddbca16647d186df5319
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2024-01-16 16:50:22 +0100

    use flex to align text inputs

--- module-03/we-are-the-champions/src/index.js
+++ module-03/we-are-the-champions/src/index.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push } from 'firebase/database'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = require('../firebase.config');
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// DOM ELEMENTS
+const inputArea = document.getElementById('text-area')
+const toInput = document.getElementById('to-input');
+const fromInput = document.getElementById('from-input');
+const publishBtn = document.getElementById('publish-btn');
+
+// EVENT HANDLERS
+publishBtn.addEventListener('click', () => {
+  console.log('Publishing endorsement');
+})
