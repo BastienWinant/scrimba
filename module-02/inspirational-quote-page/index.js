d510d1186bbeba91ba8dcae54cf285a259f3b470
Bastien Winant <bastien.winant@gmail.com>
2024-01-29 01:21:32 +0100

    add JS to dynamically render quote onto the HTML

---
+++ module-02/inspirational-quote-page/index.js
@@ -0,0 +1,49 @@
+const quoteContainer = document.getElementById('quote-container');
+
+function getRandomQuote() {
+  // get a random element from the quotes array
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
+function clearQuoteContainer() {
+  quoteContainer.innerHTML = "";
+}
+
+function createQuoteParagraph(paragraphText) {
+  // create the HTML element for the quote text
+  const pEl = document.createElement('p');
+  pEl.innerHTML = paragraphText;
+
+  // add classes for styling
+  pEl.classList.add("quote-text", "no-margin");
+
+  // return the element
+  return pEl;
+}
+
+function createQuoteSource(paragraphSource) {
+  // create the HTML element for the quote source
+  const footerEl = document.createElement('footer');
+  footerEl.innerHTML = paragraphSource;
+
+  // add classes for styling
+  footerEl.classList.add("quote-source");
+
+  // return the element
+  return footerEl;
+}
+
+function updateQuoteContainer(quoteObj) {
+  const quoteParagraph = createQuoteParagraph(quoteObj['quote']);
+  const quoteSource = createQuoteSource(quoteObj.author);
+
+  quoteContainer.appendChild(quoteParagraph);
+  quoteContainer.appendChild(quoteSource);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  clearQuoteContainer();
+  const quoteObject = getRandomQuote();
+  updateQuoteContainer(quoteObject);
+})
