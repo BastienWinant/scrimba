539bb3f0e0a29c93f6308afe03f59b473c562c62
Bastien Winant <bastien.winant@gmail.com>
2024-02-02 10:05:22 +0100

    add loader dependencies and create webpack config file

---
+++ module-02/inspirational-quote-page/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv()
+  ],
+};
