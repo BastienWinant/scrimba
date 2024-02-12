e3d232a2df80fd1c0834510ff88fc0e8ca7fa734
Bastien Winant <bastien.winant@gmail.com>
2024-02-12 16:41:49 +0100

    implement display update up key click

--- module-03/send-to-pager/index.js
+++ module-03/send-to-pager/index.js
@@ -3,3 +3,30 @@ const pagerDisplay = document.getElementById("pager-display");
 const phoneDisplay = document.getElementById("phone-display");
 const resetBtn = document.getElementById("reset-btn");
 const sendBtn = document.getElementById("send-btn");
+const keyEls = document.getElementsByClassName("key");
+
+// HELPER FUNCTIONS
+/**
+ * Clears the supplied element's inner HTML
+ * @param {*} element DOM element to be cleared
+ */
+function clearElement(element) {
+  element.innerHTML = "";
+}
+
+function updatePhoneDisplay(value) {
+  phoneDisplay.textContent += value;
+}
+
+// CALLBACK FUNCTIONS
+Array.from(keyEls).forEach((keyEl) => {
+  keyEl.addEventListener("click", (evt) => {
+    const keyVal = evt.target.textContent;
+    updatePhoneDisplay(keyVal);
+  })
+})
+
+resetBtn.addEventListener("click", () => {
+  clearElement(pagerDisplay);
+  clearElement(phoneDisplay);
+});
