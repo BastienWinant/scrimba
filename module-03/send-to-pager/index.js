794bf1eb7b7b5142dffce9e233942beab6b7654e
Bastien Winant <bastien.winant@gmail.com>
2024-02-12 16:12:48 +0100

    store DOM elements in JS variables

---
+++ module-03/send-to-pager/index.js
@@ -0,0 +1,5 @@
+// DOM ELEMENTS
+const pagerDisplay = document.getElementById("pager-display");
+const phoneDisplay = document.getElementById("phone-display");
+const resetBtn = document.getElementById("reset-btn");
+const sendBtn = document.getElementById("send-btn");
