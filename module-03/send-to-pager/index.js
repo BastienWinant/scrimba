992df684b48196ce0294b1c751aa507d28b4d50a
Bastien Winant <bastien.winant@gmail.com>
2024-02-12 17:01:33 +0100

    implement send button callback function

--- module-03/send-to-pager/index.js
+++ module-03/send-to-pager/index.js
@@ -8,25 +8,45 @@ const keyEls = document.getElementsByClassName("key");
 // HELPER FUNCTIONS
 /**
  * Clears the supplied element's inner HTML
- * @param {*} element DOM element to be cleared
+ * @param {DOM element ref} element DOM element to be cleared
  */
 function clearElement(element) {
   element.innerHTML = "";
 }
 
+/**
+ * Append the input value to the display's inner content
+ * @param {string} value the text to be appended to the existing inner content
+ */
 function updatePhoneDisplay(value) {
-  phoneDisplay.textContent += value;
+  phoneDisplay.innerText += value;
+}
+
+/**
+ * Replace the display's inner text with the input value
+ * @param {str} value the new text content to be displayed
+ */
+function updatePagerDisplay(value) {
+  pagerDisplay.innerText = value;
 }
 
 // CALLBACK FUNCTIONS
 Array.from(keyEls).forEach((keyEl) => {
   keyEl.addEventListener("click", (evt) => {
-    const keyVal = evt.target.textContent;
+    clearElement(pagerDisplay);
+    const keyVal = evt.target.innerText;
     updatePhoneDisplay(keyVal);
   })
 })
 
 resetBtn.addEventListener("click", () => {
-  clearElement(pagerDisplay);
+  // clear the displays
   clearElement(phoneDisplay);
+});
+
+sendBtn.addEventListener("click", () => {
+  // get the phone input value
+  const phoneNumber = phoneDisplay.innerText;
+  // updatePagerDisplay(phoneNumber);
+  setTimeout(updatePagerDisplay, 2000, phoneNumber);
 });
