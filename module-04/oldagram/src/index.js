bb17ce2d514f5a9982506998914d7c4e0e9b2f7e
Bastien Winant <bastien.winant@gmail.com>
2024-02-21 10:10:50 +0100

    import font from local files

--- module-04/oldagram/src/index.js
+++ module-04/oldagram/src/index.js
@@ -1,6 +1,8 @@
+import './style.css'
+
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = `<h1>Hello World</h1>`;
+  element.innerHTML = `<h1 class="black-border">Hello World</h1>`;
   return element;
 }
 
