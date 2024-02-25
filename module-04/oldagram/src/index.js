64b6dca0cd0e42a3a0d472b436d6ba4739a6eb8c
Bastien Winant <bastien.winant@gmail.com>
2024-02-25 22:39:49 +0100

    add sign up form

--- module-04/oldagram/src/index.js
+++ module-04/oldagram/src/index.js
@@ -1,8 +1,17 @@
-function component() {
-  const element = document.createElement("h1");
-  element.innerHTML = "Hello World";
+import './index.css';
 
-  return element;
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { firebaseConfig } from "../firebase.config"
 
-document.body.appendChild(component());
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// SIGNUP FORM
+const signupEmail = document.getElementById("signup-email");
+const signupPassword = document.getElementById("signup-password");
+const signupBtn= document.getElementById("signup-btn");
+
+console.log(signupEmail);
