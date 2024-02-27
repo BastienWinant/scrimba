a2ca7dbd6ffeb048e5e10ef581689fed44310a64
Bastien Winant <bastien.winant@gmail.com>
2024-02-27 14:33:57 +0100

    import and apply font

--- module-04/oldagram/src/index.js
+++ module-04/oldagram/src/index.js
@@ -1,10 +1,2 @@
 import './style.css'
 
-function component() {
-  const element = document.createElement("h2");
-  element.innerHTML = "Hello World";
-  element.classList.add("black-border");
-  return element;
-}
-
-document.body.appendChild(component());
