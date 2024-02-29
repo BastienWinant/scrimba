ff1804c027737de091c96e8b053d7823c354b9b5
Bastien Winant <bastien.winant@gmail.com>
2024-02-29 14:58:24 +0100

    use html templating

--- module-04/oldagram/webpack.config.js
+++ module-04/oldagram/webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   // The entry point file described above
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/templates/index.html'
+    }),
+  ],
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true,
   },
   module: {
     rules: [
@@ -27,5 +34,8 @@ module.exports = {
   },
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
 };
