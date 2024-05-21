618c1b14dfe8bfdc766c470c6f31298fbc395f41
Bastien Winant <bastien.winant@gmail.com>
2024-05-21 18:56:50 +0200

    put click event listener on container element

--- module-05/restaurant-ordering/src/index.js
+++ module-05/restaurant-ordering/src/index.js
@@ -4,7 +4,7 @@ import menuData from '/assets/data/data.json'
 function displayMenu() {
   const menuContainer = document.querySelector('.menu')
   menuContainer.innerHTML = menuData.map(menuItem => {
-    const { name, ingredients, id, price, emoji } = menuItem
+    const { name, ingredients, id, price, emoji, orderCount } = menuItem
 
     return `<div class="menu-item black-border">
               <p class="no-margin item-graphic black-border">${emoji}</p>
@@ -13,11 +13,15 @@ function displayMenu() {
                 <p class="item-ingredients no-margin black-border">${ingredients.join(", ")}</p>
                 <p class="item-price no-margin black-border">$${price}</p>
               </div>
-              <button class="add-button" data-item-id="${id}">
-                <i class="fa-solid fa-plus fa-lg"></i>
-              </button>
+              <i class="add-button fa-solid fa-plus fa-lg" data-item-id="${id}"></i>
             </div>`
   }).join('\n')
 }
+
+document.querySelector('.menu').addEventListener('click', (e) => {
+  if (e.target.classList.contains('add-button')) {
+    console.log(e.target.dataset.itemId)
+  }
+})
 
 displayMenu()
