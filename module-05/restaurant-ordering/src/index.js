0dd06338ad2b52ee0cdde52ef32122d2092b4110
Bastien Winant <bastien.winant@gmail.com>
2024-05-21 15:17:00 +0200

    create card div for menu items

--- module-05/restaurant-ordering/src/index.js
+++ module-05/restaurant-ordering/src/index.js
@@ -1,4 +1,23 @@
 import './styles.css'
-import menuData from '/data.json'
+import menuData from '/assets/data/data.json'
 
-console.log(data)
+function displayMenu() {
+  const menuContainer = document.querySelector('.menu')
+  menuContainer.innerHTML = menuData.map(menuItem => {
+    const { name, ingredients, id, price, emoji } = menuItem
+
+    return `<div class="menu-item black-border">
+              <p class="no-margin item-graphic black-border">${emoji}</p>
+              <div class="menu-item-info">
+                <h3 class="item-name no-margin black-border">${name}</h3>
+                <p class="item-ingredients no-margin black-border">${ingredients.join(", ")}</p>
+                <p class="item-price no-margin black-border">$${price}</p>
+              </div>
+              <button class="add-button" data-item-id="${id}">
+                <i class="fa-solid fa-plus fa-lg"></i>
+              </button>
+            </div>`
+  }).join('\n')
+}
+
+displayMenu()
