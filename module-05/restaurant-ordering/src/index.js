e60abe4185c788f2c8730ec5832f2591e0d9b5b0
Bastien Winant <bastien.winant@gmail.com>
2024-05-22 12:43:11 +0200

    add total price to order summary

--- module-05/restaurant-ordering/src/index.js
+++ module-05/restaurant-ordering/src/index.js
@@ -15,11 +15,12 @@ function displayMenu() {
               </div>
               <div class="order-btns">
                 <i class="round-btn add-btn fa-solid fa-plus fa-lg" data-item-id="${id}"></i>
-                <i class="round-btn add-btn fa-solid fa-minus fa-lg" data-item-id="${id}"></i>
+                <i class="round-btn remove-btn fa-solid fa-minus fa-lg" data-item-id="${id}"></i>
               </div>
             </div>`
   }).join('\n')
 }
+
 
 function addToOrder(itemId) {
   const orderObj = localStorage.getItem('order') ? JSON.parse(localStorage.getItem('order')) : {}
@@ -30,6 +31,17 @@ function addToOrder(itemId) {
 function removeFromOrder(itemId) {
   const orderObj = localStorage.getItem('order') ? JSON.parse(localStorage.getItem('order')) : {}
   orderObj[itemId] = orderObj[itemId] ? orderObj[itemId] - 1 : 0
+
+  if (orderObj[itemId] <= 0) {
+    delete orderObj[itemId]
+  }
+
+  localStorage.setItem('order', JSON.stringify(orderObj))
+}
+
+function deleteFromOrder(itemId) {
+  const orderObj = localStorage.getItem('order') ? JSON.parse(localStorage.getItem('order')) : {}
+  delete orderObj[itemId]
   localStorage.setItem('order', JSON.stringify(orderObj))
 }
 
@@ -39,30 +51,43 @@ function displayOrder() {
     document.querySelector('.order-items').innerHTML = '';
 
     let orderSummaryHTML = ''
+
+    let totalOrderPrice = 0;
+
     for (const [itemId, orderCount] of Object.entries(orderObj)) {
       const item = menuData.find(item => item.id == itemId)
-      const totalPrice = item.price * orderCount
+      const itemOrderPrice = item.price * orderCount
+
+      totalOrderPrice += itemOrderPrice
 
       orderSummaryHTML += `<div class="order-item">
                             <p class="no-margin order-item-name">${item.name} <span class="order-item-count">(x${orderCount})</span></p>
                             <button class="no-padding delete-btn" type="button">remove</button>
-                            <p class="no-margin order-item-price">$${totalPrice}</p>
+                            <p class="no-margin order-item-price">$${itemOrderPrice}</p>
                           </div>\n`
-
     }
+
+    orderSummaryHTML += '<hr>\n'
+
+    orderSummaryHTML += `<div class="order-item">
+                          <p class="no-margin order-item-name">Total Price:</p>
+                          <p class="no-margin order-item-price">$${totalOrderPrice}</p>
+                        </div>`
     
     document.querySelector('.order-items').innerHTML = orderSummaryHTML
   }
 }
 
+// handle click events on the app container
 document.querySelector('.app-container').addEventListener('click', (e) => {
   if (e.target.classList.contains('add-btn')) {
     addToOrder(e.target.dataset.itemId)
-    displayMenu()
     displayOrder()
-  }
-
-  if (e.target.classList.contains('delete-btn')) {
+  } else if (e.target.classList.contains('remove-btn')) {
+    removeFromOrder(e.target.dataset.itemId)
+    displayOrder()
+  } else if (e.target.classList.contains('delete-btn')) {
+    deleteFromOrder(e.target.dataset.itemId)
     displayOrder();
   }
 })
