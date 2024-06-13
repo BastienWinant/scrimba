e110d1ff2138e6c3dfc2585eb35716dce580e271
Bastien Winant <bastien.winant@gmail.com>
2024-06-13 15:44:10 +0200

    import font

--- module-06/learning-journal/webpack.config.js
+++ module-06/learning-journal/webpack.config.js
@@ -7,4 +7,21 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'Fonts': path.resolve(__dirname, 'assets/fonts/')
+    }
+  }
 }
