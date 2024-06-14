563fee3298ea982c95c79b39d57ce81801bf655a
Bastien Winant <bastien.winant@gmail.com>
2024-06-14 09:45:05 +0200

    add comments to app.js file

--- module-06/learning-journal/app.js
+++ module-06/learning-journal/app.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const router = require('./routes/main')
 
+const app = express();
+
+// set templating engine
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
+// mount routers
 app.use("/", router);
 
+// start server
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 })
