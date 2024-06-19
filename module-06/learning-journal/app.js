9c1c9a0ff3341091f79071ab46ae66c59a859e98
Bastien Winant <bastien.winant@gmail.com>
2024-06-19 23:49:06 +0200

    check for app environment in app js

--- module-06/learning-journal/app.js
+++ module-06/learning-journal/app.js
@@ -5,27 +5,42 @@ const sqlite3 = require('sqlite3')
 
 const app = express();
 
-// connect to database
-const db = new sqlite3.Database('data.db', (err) => {
-  if (err) {
-    return console.log(err.message)
-  }
+// // connect to database
+// const db = new sqlite3.Database('data.db', (err) => {
+//   if (err) {
+//     return console.log(err.message)
+//   }
 
-  console.log("Connect to the database")
-})
+//   console.log("Connect to the database")
+// })
 
-// close the db connection
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+// // close the db connection
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+// });
 
 // set templating engine
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
+
+// Setup for production environment
+if ('production' == app.get('env')) {
+  app.get('/', function(req, res) {
+    res.send("Production Environment")
+  });
+}
+
+// Setup for development environment
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+  app.get('/', function(req, res) {
+    res.send("Dev Environment")
+  });
+}
 
 // mount routers
 app.use("/", router);
