e02eb252148612fd8ab9c2b45d72d48804f6583e
Bastien Winant <bastien.winant@gmail.com>
2024-06-22 00:16:28 +0200

    Add articles table in schema file

--- module-06/learning-journal/db_schema.sql
+++ module-06/learning-journal/db_schema.sql
@@ -7,16 +7,27 @@ BEGIN TRANSACTION;
 -- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL
+  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS email_accounts (
-    email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
-    user_id  INT, --the user that the email account belongs to
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+	email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	email_address TEXT NOT NULL,
+	user_id  INT, --the user that the email account belongs to
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+CREATE TABLE IF NOT EXISTS articles (
+	article_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id  INT, --the user who wrote the article
+	title TEXT NOT NULL,
+	article_text TEXT NOT NULL,
+	creation_date TEXT NOT NULL,
+	modification_date TEXT,
+	edited SMALLINT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
+)
 
 -- Insert default data (if necessary here)
 
