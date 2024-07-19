d29ec182e6f8a815d08b5bf4803c309b48d7852d
Bastien Winant <bastien.winant@gmail.com>
2024-07-19 23:33:50 +0200

    remove unused EJS parser

--- module-06/learning-journal/index.js
+++ module-06/learning-journal/index.js
@@ -3,79 +3,34 @@
 * This is your main app entry point
 */
 
-// Set up express, bodyparser and EJS
+// Set up express & bodyparser
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs'); // set the app to use ejs for rendering
-app.use(express.static(path.resolve(__dirname, 'public'))); // set location of static files
-
-// Set up session configuration
-const session = require('express-session');
-const store = new session.MemoryStore(); // default server-side session storage
-const sessionConfig = session({
-	secret: "f4z4gs$Gcg",
-	resave: false,
-	saveUninitialized: false,
-	store,
-	cookie: {
-		maxAge: 86400000,
-		secure: true,
-		sameSite: "none"
-	}
-})
-app.set("trust proxy", 1);
-app.use(sessionConfig)
-
-// Set up authentication
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(
-	function(username, password, done) {
-    db.users.findByUsername(username, (err, user) => {
-      if (err) return done(err);
-
-      if (!user) return done(null, false);
-
-      if (user.password != password) return done(null, false);
-
-      return done(null, user);
-    });
-  }
-))
+app.use(express.static(path.resolve(__dirname, 'public'))) // set location of static files
 
 // Set up SQLite
 // Items in the global namespace are accessible throught out the node application
 const sqlite3 = require('sqlite3').verbose();
 global.db = new sqlite3.Database('./database.db',function(err){
-	if(err){
-		console.error(err);
-		process.exit(1); // bail out we can't connect to the DB
-	} else {
-		console.log("Database connected");
-		global.db.run("PRAGMA foreign_keys=ON"); // tell SQLite to pay attention to foreign key constraints
-	}
+    if(err){
+        console.error(err);
+        process.exit(1); // bail out we can't connect to the DB
+    } else {
+        console.log("Database connected");
+        global.db.run("PRAGMA foreign_keys=ON"); // tell SQLite to pay attention to foreign key constraints
+    }
 });
 
-const ensureAuthentication = (req, res, next) => {
-	// Check for the authorized property within the session
-	req.session.authenticated
-									? res.next()
-									: res.stats(403).json({ msg: "You're not authorized to view this page" })
-}
-
-// Pass the Express instance to the routes table
-require('./routes')(app);
+// Pass the Express instance to the routes module
+require('./routes')(app)
 
 
 // Make the web application listen for HTTP requests
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
--- module-06/learning-journal/routes.js
+++ module-06/learning-journal/routes.js
@@ -1,15 +1,12 @@
 // Load the route handlers
 const user = require('./handlers/users');
-const auth = require('./handlers/auth');
 
 module.exports = function(app) {
   // Handle requests to the home page 
   app.get('/', (req, res) => {
-      res.send('Hello World!')
+    res.send('Hello World!')
   });
-
-  app.use('/user', auth);
 
   // Add all the route handlers in usersRoutes to the app under the path /users
   app.use('/users', user);
-};
+}
