cbe7a43cfa63eee616b238b17ed711e630aeeab0
Bastien Winant <bastien.winant@gmail.com>
2024-07-20 00:29:04 +0200

    add dummy users data

--- module-06/learning-journal/db_schema.sql
+++ module-06/learning-journal/db_schema.sql
@@ -4,7 +4,7 @@ PRAGMA foreign_keys=ON;
 
 BEGIN TRANSACTION;
 
--- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
+-- Create tables
 
 CREATE TABLE IF NOT EXISTS users (
 	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,17 +22,12 @@ CREATE TABLE IF NOT EXISTS articles (
 	FOREIGN KEY (author_id) REFERENCES users(user_id)
 );
 
--- Insert default data (if necessary here)
+-- Insert default data
 
 -- Set up three users
-INSERT INTO users ('user_name') VALUES ('Simon Star');
-INSERT INTO users ('user_name') VALUES ('Dianne Dean');
-INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
-
--- Give Simon two email addresses and Diane one, but Harry has none
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
+INSERT INTO user ('user_name', 'email_address', 'password_hash') VALUES ('Simon', 'simon@gmail.com', '$2a$10$q1Cym9bgIT.1GRqU8maHVedRnrtmf4bHOuM1UAos6cCV7SLJ1ZsGS'); -- password: test_1 
+INSERT INTO user ('user_name', 'email_address', 'password_hash') VALUES ('Alex', 'alex@hotmail.com', '$2a$10$0WRJNK52UpkymSHgs0DOgOZgIj/B/hH1MiHEcJEy4v41wBuSBvwca'); -- password: test_2
+INSERT INTO user ('user_name', 'email_address', 'password_hash') VALUES ('Dianne', 'dianne@yahoo.co.uk', '$2a$10$dH6xdXeH9BdojS5qLhBhEu20S2VzdtjBgHolITgoYn1mJx6oXplsa');  -- password: test_3
 
 COMMIT;
 
