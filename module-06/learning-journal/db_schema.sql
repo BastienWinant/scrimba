9e52b2ac5aa13f555941122d3cfcadee7ef70ec6
Bastien Winant <bastien.winant@gmail.com>
2024-07-19 23:58:02 +0200

    add password variable to accounts table

--- module-06/learning-journal/db_schema.sql
+++ module-06/learning-journal/db_schema.sql
@@ -7,15 +7,16 @@ BEGIN TRANSACTION;
 -- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL
+	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS email_accounts (
-    email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
-    user_id  INT, --the user that the email account belongs to
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+	email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	email_address TEXT NOT NULL,
+	user_id  INT, --the user that the email account belongs to
+	email_account_password TEXT NOT NULL, 
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 -- Insert default data (if necessary here)
