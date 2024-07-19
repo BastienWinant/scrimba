a6adf7a0565d54170981c2ba345ca5cc3b836951
Bastien Winant <bastien.winant@gmail.com>
2024-07-19 23:51:31 +0200

    add password hashing

--- module-06/learning-journal/index.js
+++ module-06/learning-journal/index.js
@@ -3,13 +3,14 @@
 * This is your main app entry point
 */
 
-// Set up express & bodyparser
+// Set up express, bodyparser and EJS
 const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs'); // set the app to use ejs for rendering
 app.use(express.static(path.resolve(__dirname, 'public'))) // set location of static files
 
 // Set up SQLite
--- module-06/learning-journal/routes.js
+++ module-06/learning-journal/routes.js
@@ -1,12 +1,27 @@
+var bcrypt = require('bcryptjs');
+
 // Load the route handlers
 const user = require('./handlers/users');
 
 module.exports = function(app) {
   // Handle requests to the home page 
-  app.get('/', (req, res) => {
+  app.get('/', (req, res, next) => {
     res.send('Hello World!')
   });
 
   // Add all the route handlers in usersRoutes to the app under the path /users
   app.use('/users', user);
+
+  app.get('/signup', (req, res, next) => {
+    res.render('signup.ejs')
+  })
+
+  app.post('/signup', (req, res, next) => {
+    const {username, email, password} = req.body;
+    
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    res.send(`${username} ${email} ${password} | ${hash}`)
+  })
 }
