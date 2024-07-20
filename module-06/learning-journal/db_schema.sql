278f3100c6518c895347529a15a524d02d7ed18e
Bastien Winant <bastien.winant@gmail.com>
2024-07-20 15:09:21 +0200

    add back email in signup form

--- module-06/learning-journal/db_schema.sql
+++ module-06/learning-journal/db_schema.sql
@@ -9,7 +9,7 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	username TEXT NOT NULL,
-	-- email_address TEXT NOT NULL,
+	email TEXT NOT NULL,
 	hashed_password BLOB NOT NULL,
 	salt BLOB NOT NULL
 );
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS articles (
 -- Insert default data
 
 -- Set up three users
-INSERT INTO users ('username', 'hashed_password', 'salt') VALUES ('Simon', '$2a$10$q1Cym9bgIT.1GRqU8maHVedRnrtmf4bHOuM1UAos6cCV7SLJ1ZsGS', '10'); -- password: test_1, salt: 10
-INSERT INTO users ('username', 'hashed_password', 'salt') VALUES ('Alex', '$2a$10$0WRJNK52UpkymSHgs0DOgOZgIj/B/hH1MiHEcJEy4v41wBuSBvwca', '10'); -- password: test_2, salt: 10
-INSERT INTO users ('username', 'hashed_password', 'salt') VALUES ('Dianne', '$2a$10$dH6xdXeH9BdojS5qLhBhEu20S2VzdtjBgHolITgoYn1mJx6oXplsa', '10');  -- password: test_3, salt: 10
+INSERT INTO users ('username', 'email', 'hashed_password', 'salt') VALUES ('Simon', 'simon@gmail.com', '$2a$10$q1Cym9bgIT.1GRqU8maHVedRnrtmf4bHOuM1UAos6cCV7SLJ1ZsGS', '10'); -- password: test_1, salt: 10
+INSERT INTO users ('username', 'email', 'hashed_password', 'salt') VALUES ('Alex', 'alex@yahoo.com', '$2a$10$0WRJNK52UpkymSHgs0DOgOZgIj/B/hH1MiHEcJEy4v41wBuSBvwca', '10'); -- password: test_2, salt: 10
+INSERT INTO users ('username', 'email', 'hashed_password', 'salt') VALUES ('Dianne', 'dianne@london.ac.uk', '$2a$10$dH6xdXeH9BdojS5qLhBhEu20S2VzdtjBgHolITgoYn1mJx6oXplsa', '10');  -- password: test_3, salt: 10
 
 COMMIT;
 
--- module-06/learning-journal/handlers/auth.js
+++ module-06/learning-journal/handlers/auth.js
@@ -57,20 +57,22 @@ router.get('/signup', (req, res, next) => {
 });
 
 router.post('/signup', function(req, res, next) {
-  const {username, password} = req.body;
+  const {username, email, password} = req.body;
   const salt = crypto.randomBytes(16);
 
   crypto.pbkdf2(password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
     if (err) { return next(err); }
-    db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+    db.run('INSERT INTO users (username, email, hashed_password, salt) VALUES (?, ?, ?, ?)', [
       username,
+      email,
       hashedPassword,
       salt
     ], function(err) {
       if (err) return next(err);
       const user = {
         id: this.lastID,
-        username: username
+        username: username,
+        email: email
       };
       req.login(user, function(err) {
         if (err) return next(err);
