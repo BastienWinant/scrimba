e0a249e1d75d54ad138d3b332d345e67bbaca604
Bastien Winant <bastien.winant@gmail.com>
2024-07-20 14:35:41 +0200

    rename users table columns

--- module-06/learning-journal/db_schema.sql
+++ module-06/learning-journal/db_schema.sql
@@ -7,10 +7,11 @@ BEGIN TRANSACTION;
 -- Create tables
 
 CREATE TABLE IF NOT EXISTS users (
-	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	user_name TEXT NOT NULL,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	username TEXT NOT NULL,
 	email_address TEXT NOT NULL,
-	password_hash TEXT NOT NULL
+	hashed_password BLOB NOT NULL,
+	salt BLOB NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS articles (
@@ -25,9 +26,9 @@ CREATE TABLE IF NOT EXISTS articles (
 -- Insert default data
 
 -- Set up three users
-INSERT INTO user ('user_name', 'email_address', 'password_hash') VALUES ('Simon', 'simon@gmail.com', '$2a$10$q1Cym9bgIT.1GRqU8maHVedRnrtmf4bHOuM1UAos6cCV7SLJ1ZsGS'); -- password: test_1 
-INSERT INTO user ('user_name', 'email_address', 'password_hash') VALUES ('Alex', 'alex@hotmail.com', '$2a$10$0WRJNK52UpkymSHgs0DOgOZgIj/B/hH1MiHEcJEy4v41wBuSBvwca'); -- password: test_2
-INSERT INTO user ('user_name', 'email_address', 'password_hash') VALUES ('Dianne', 'dianne@yahoo.co.uk', '$2a$10$dH6xdXeH9BdojS5qLhBhEu20S2VzdtjBgHolITgoYn1mJx6oXplsa');  -- password: test_3
+INSERT INTO users ('username', 'email_address', 'hashed_password', 'salt') VALUES ('Simon', 'simon@gmail.com', '$2a$10$q1Cym9bgIT.1GRqU8maHVedRnrtmf4bHOuM1UAos6cCV7SLJ1ZsGS', '10'); -- password: test_1, salt: 10
+INSERT INTO users ('username', 'email_address', 'hashed_password', 'salt') VALUES ('Alex', 'alex@hotmail.com', '$2a$10$0WRJNK52UpkymSHgs0DOgOZgIj/B/hH1MiHEcJEy4v41wBuSBvwca', '10'); -- password: test_2, salt: 10
+INSERT INTO users ('username', 'email_address', 'hashed_password', 'salt') VALUES ('Dianne', 'dianne@yahoo.co.uk', '$2a$10$dH6xdXeH9BdojS5qLhBhEu20S2VzdtjBgHolITgoYn1mJx6oXplsa', '10');  -- password: test_3, salt: 10
 
 COMMIT;
 
