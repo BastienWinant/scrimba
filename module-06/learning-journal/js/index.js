da1adcdc7b8a6769fecf05b026fa280d873cd397
Bastien Winant <bastien.winant@gmail.com>
2024-07-22 22:25:23 +0200

    render grid with javascript

--- module-06/learning-journal/js/index.js
+++ module-06/learning-journal/js/index.js
@@ -1,9 +1,43 @@
+let articlesToDisplay = 6;
+
 document.body.addEventListener('click', (e) => {
   if ((e.target.classList.contains('nav-expand-btn')) || (e.target.classList.contains('nav-collapse-btn'))) {
     document.querySelector('#header-nav').classList.toggle('expanded');
-  }
-})
+  } else if (e.target.classList.contains('grid-expand-btn')) {
+    articlesToDisplay += 3;
+    displayArticles(articlesToDisplay);
+  } 
+});
 
 window.addEventListener('resize', () => {
   document.querySelector('#header-nav').classList.remove('expanded');
-})
+});
+
+
+const displayArticles = n => {
+  const articleGrid = document.querySelector('#article-grid');
+
+  fetch('../data/articles.json')
+    .then(response => response.json())
+    .then(data => {
+      articleGrid.innerHTML = data.slice(0, n).map(articleObj => {
+        return `<a class="article-card" href="#">
+          <div class="masked">
+            <div class="mask"></div>
+            <img class="img article-img" src="${articleObj.imgUrl}" alt="Illustrative image" >
+          </div>
+          <div class="article-info">
+            <h2 class="article-title">${articleObj.title}</h2>
+            <p class="article-date">${articleObj.date}</p>
+            <p class="article-blurb">${articleObj.text}</p>
+          </div>
+        </a>`
+      }).join('\n');
+
+      if (n >= data.length) {
+        document.querySelector('#grid-expand-btn-container').style.display = "none";
+      }
+    });
+}
+
+displayArticles(articlesToDisplay);
