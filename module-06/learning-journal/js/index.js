da3deff48867630dbe9357d3045536e14b5774f9
Bastien Winant <bastien.winant@gmail.com>
2024-07-22 18:02:12 +0200

    refactor header and add footer

--- module-06/learning-journal/js/index.js
+++ module-06/learning-journal/js/index.js
@@ -1,10 +1,9 @@
-document.body.addEventListener('click', e => {
-  if ((e.target.classList.contains('nav-expand-btn')) || (e.target.classList.contains('nav-collapse-btn')) ){
+document.body.addEventListener('click', (e) => {
+  if ((e.target.classList.contains('nav-expand-btn')) || (e.target.classList.contains('nav-collapse-btn'))) {
     document.querySelector('#header-nav').classList.toggle('expanded');
   }
 })
 
-window.addEventListener('resize', e => {
-  console.log('testing');
+window.addEventListener('resize', () => {
   document.querySelector('#header-nav').classList.remove('expanded');
 })
