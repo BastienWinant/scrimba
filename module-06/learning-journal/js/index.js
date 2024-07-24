732433a6e7275b635d9d23da965a0174a1b00288
Bastien Winant <bastien.winant@gmail.com>
2024-07-24 21:23:06 +0200

    add hero section to homepage and render with js

--- module-06/learning-journal/js/index.js
+++ module-06/learning-journal/js/index.js
@@ -5,7 +5,7 @@ document.body.addEventListener('click', (e) => {
     document.querySelector('#header-nav').classList.toggle('expanded');
   } else if (e.target.classList.contains('grid-expand-btn')) {
     articlesToDisplay += 3;
-    displayArticles(articlesToDisplay);
+    displayArticles(articlesToDisplay + 1);
   } 
 });
 
@@ -13,33 +13,47 @@ window.addEventListener('resize', () => {
   document.querySelector('#header-nav').classList.remove('expanded');
 });
 
-
-const displayArticles = n => {
-  const articleGrid = document.querySelector('#article-grid');
-
-  fetch('../data/articles.json')
-    .then(response => response.json())
-    .then(data => {
-      articleGrid.innerHTML = data.slice(0, n).map(articleObj => {
-        const cardText = articleObj.text.length <= 200 ?
-          articleObj.text : articleObj.text.slice(0, 201) + '...';
-        return `<a class="article-card" href="#">
-          <div class="masked">
-            <div class="mask"></div>
-            <img class="img article-img" src="${articleObj.imgUrl}" alt="Illustrative image" >
-          </div>
-          <div class="article-info">
-            <h2 class="article-title">${articleObj.title}</h2>
-            <p class="article-date">${articleObj.date}</p>
-            <p class="article-blurb">${cardText}</p>
-          </div>
-        </a>`
-      }).join('\n');
-
-      if (n >= data.length) {
-        document.querySelector('#grid-expand-btn-container').style.display = "none";
-      }
-    });
+const displayHero = (articleObj) => {
+  document.querySelector('#hero').style.backgroundImage = `url(${articleObj.imgUrl})`;
+  document.querySelector('#hero-title').innerText = articleObj.title;
+  document.querySelector('#hero-date').innerText = articleObj.date;
+  document.querySelector('#hero-blurb').innerText = articleObj.text;
 }
 
-displayArticles(articlesToDisplay);
+
+const displayArticles = (data, n) => {
+  const articleGrid = document.querySelector('#article-grid');
+
+  articleGrid.innerHTML = data.slice(1, n).map(articleObj => {
+    
+    const cardText = articleObj.text.length <= 200 ?
+      articleObj.text : articleObj.text.slice(0, 201) + '...';
+    
+    return `<a class="article-link" href="#">
+      <div class="masked">
+        <div class="mask"></div>
+        <img class="img article-img" src="${articleObj.imgUrl}" alt="Illustrative image" >
+      </div>
+      <hgroup class="article-info black-border">
+        <h2 class="article-title">${articleObj.title}</h2>
+        <p class="article-date">${articleObj.date}</p>
+        <p class="article-blurb">${cardText}</p>
+      </hgroup>
+    </a>`
+  }).join('\n');
+
+  if (n >= data.length) {
+    document.querySelector('#grid-expand-btn-container').style.display = "none";
+  }
+}
+
+const renderPage = () => {
+  fetch('../data/articles.json')
+  .then(response => response.json())
+  .then(data => {
+    displayHero(data[0]);
+    displayArticles(data, articlesToDisplay + 1);
+  })
+}
+
+renderPage();
