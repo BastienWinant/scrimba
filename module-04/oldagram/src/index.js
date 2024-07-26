57db157e692b29a43d9e3026f7e228fb320814eb
Bastien Winant <bastien.winant@gmail.com>
2024-07-26 22:40:38 +0200

    generate post image

--- module-04/oldagram/src/index.js
+++ module-04/oldagram/src/index.js
@@ -3,7 +3,15 @@ import './style.css';
 import { Header } from "./components/header/header";
 import { Post } from "./components/post/post"
 
+console.log(Post());
+
 document.body.insertAdjacentElement(
   "afterbegin",
   Header()
 );
+
+const mainEl = document.querySelector(".main");
+mainEl.insertAdjacentElement(
+  "afterbegin",
+  Post()
+)
