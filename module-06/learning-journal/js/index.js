7658820340404eca6132ae39d12dcb42810b7348
Bastien Winant <bastien.winant@gmail.com>
2024-07-26 15:25:22 +0200

    about page rendering function

--- module-06/learning-journal/js/index.js
+++ module-06/learning-journal/js/index.js
@@ -9,10 +9,11 @@ document.addEventListener('click', (e) => {
     expandArticlesGrid();
   } else if (e.target.id === "home-btn" || e.target.id === "logo-btn") {
     renderHomePage();
-    collapseNavbar();
   } else if (e.target.closest(".article-btn")) {
     const articleId = e.target.closest(".article-btn").dataset.articleId;
     renderArticlePage(articleId);
+  } else if (e.target.id === "about-btn") {
+    renderAboutPage();
   }
 });
 
@@ -32,6 +33,7 @@ const toggleNavbar = () => {
 const clearPage = () => {
   document.querySelector("#main").innerHTML = "";
   articlesToDisplay = 3;
+  collapseNavbar();
 }
 
 const addHeroSection = (articleObj) => {
@@ -154,6 +156,28 @@ const displayArticle = (articleObj) => {
     </article>`
 }
 
+const addAboutSection = () => {
+  const mainEl = document.querySelector("#main");
+  mainEl.innerHTML +=
+    `<section class="about">
+      <header class="about-header">
+        <img class="about-img" src="https://images.unsplash.com/photo-1721804978061-2c23db2b5e4c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D" alt="Avatar headshot">
+        <hgroup>
+          <h1 class="about-header-title">Hi there! My name is Roku and welcome to my learning journal.</h1>
+          <p class="about-header-intro">After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.</p>
+        </hgroup>
+      </header>
+      <section class="article-body">
+        <h2 class="article-subtitle">How I stay committed to learning</h2>
+        <p class="article-text">I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.</p>
+        <p class="article-text">While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.</p>
+        <h2 class="article-subtitle">How I got started</h2>
+        <p class="article-text">I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.</p>
+        <p class="article-text">That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.</p>
+      </section>
+    </section>`;
+}
+
 const renderHomePage = () => {
   clearPage();
 
@@ -175,6 +199,24 @@ const renderHomePage = () => {
         addGridExpandBtn();
       };
     });
+}
+
+const renderAboutPage = () => {
+  clearPage();
+  addAboutSection();
+
+  fetch('../data/articles.json')
+    .then(response => response.json())
+    .then(articles => {
+      // create an empty grid container
+      addCardGrid();
+
+      addCardGridHeader();
+
+      // display the article cards
+      fillCardGrid(articles);
+      
+    })
 }
 
 const renderArticlePage = (articleId) => {
