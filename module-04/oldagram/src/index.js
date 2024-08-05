5c9c741600ad24d62c00e82710b26413da87a0fd
Bastien Winant <bastien.winant@gmail.com>
2024-08-05 16:54:06 +0200

    connect to unsplash api

--- module-04/oldagram/src/index.js
+++ module-04/oldagram/src/index.js
@@ -2,47 +2,64 @@ import './styles.css';
 import { Header } from './components/header/index';
 import { Post, updateLikes, updateBookmark } from './components/post/index'
 import posts from './assets/posts.json';
+import { createApi } from 'unsplash-js';
 
 document.querySelector('header').insertAdjacentHTML(
   "afterbegin",
   Header()
 );
 
-document.querySelector('.main').innerHTML = posts.map(postObj => Post(postObj)).join('\n');
+// document.querySelector('.main').innerHTML = posts.map(postObj => Post(postObj)).join('\n');
 
-document.querySelectorAll('.like-btn').forEach(btn => {
-  btn.addEventListener('click', e => {
-    const postEl = e.target.closest('.post');
-    const postObj = posts.find(post => post.id === postEl.dataset.postId);
+// document.querySelectorAll('.like-btn').forEach(btn => {
+//   btn.addEventListener('click', e => {
+//     const postEl = e.target.closest('.post');
+//     const postObj = posts.find(post => post.id === postEl.dataset.postId);
 
-    updateLikes(postEl, postObj)
-  })
-})
+//     updateLikes(postEl, postObj)
+//   })
+// })
 
-document.querySelectorAll('.bookmark-btn').forEach(btn => {
-  btn.addEventListener('click', e => {
-    const postEl = e.target.closest('.post');
-    const postObj = posts.find(post => post.id === postEl.dataset.postId);
+// document.querySelectorAll('.bookmark-btn').forEach(btn => {
+//   btn.addEventListener('click', e => {
+//     const postEl = e.target.closest('.post');
+//     const postObj = posts.find(post => post.id === postEl.dataset.postId);
 
-    updateBookmark(postEl, postObj)
-  })
-})
+//     updateBookmark(postEl, postObj)
+//   })
+// })
 
-// import { createApi } from 'unsplash-js';
+// on your node server
+const unsplash = createApi({
+  accessKey: process.env.ACCESS_KEY
+});
 
-// // on your node server
-// const unsplash = createApi({
-//   accessKey: process.env.ACCESS_KEY
-// });
+unsplash.photos.getRandom({
+  count: 20,
+}).then(result => {
+  if (result.errors) {
+    // handle error here
+    console.log('error occurred: ', result.errors[0]);
+  } else {
+    const posts = result.response;
+    document.querySelector('.main').innerHTML = posts.map(postObj => Post(postObj)).join('\n');
 
-// unsplash.photos.getRandom({
-//   count: 20,
-// }).then(result => {
-//   if (result.errors) {
-//     // handle error here
-//     console.log('error occurred: ', result.errors[0]);
-//   } else {
-//     const feed = result.response;
-//     console.log(feed);
-//   }
-// });
+    document.querySelectorAll('.like-btn').forEach(btn => {
+      btn.addEventListener('click', e => {
+        const postEl = e.target.closest('.post');
+        const postObj = posts.find(post => post.id === postEl.dataset.postId);
+    
+        updateLikes(postEl, postObj)
+      })
+    })
+    
+    document.querySelectorAll('.bookmark-btn').forEach(btn => {
+      btn.addEventListener('click', e => {
+        const postEl = e.target.closest('.post');
+        const postObj = posts.find(post => post.id === postEl.dataset.postId);
+    
+        updateBookmark(postEl, postObj)
+      })
+    })
+  }
+});
