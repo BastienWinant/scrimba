2084dca0d5a063c0a01a13a305602ef770bbca24
Bastien Winant <bastien.winant@gmail.com>
2024-08-13 20:33:16 +0200

    set api fetching

--- module-08/color-scheme-generator/js/index.js
+++ module-08/color-scheme-generator/js/index.js
@@ -1,4 +1,26 @@
+let colorsArr = []
+
 document.querySelector('#form').addEventListener('submit', e => {
   e.preventDefault()
-  console.log(e.target)
+  const hex = document.querySelector('#color').value.slice(1,)
+  const mode = document.querySelector('#mode').value
+  const format = document.querySelector('#format').value
+  const count = document.querySelector('#count').value
+
+  colorsArr = fetchColorScheme(hex, mode, count, format)
 })
+
+
+function fetchColorScheme(hex, mode, count, format) {
+  const baseUrl = "https://www.thecolorapi.com"
+  const endpoint = "scheme"
+
+  const fullUrl = `${baseUrl}/${endpoint}?hex=${hex}&mode=${mode}&count=${count}`
+
+  fetch(fullUrl, { method: 'GET'})
+    .then(response => response.json())
+    .then(data => {
+      const colorsArray = data.colors.map(color => color[format].value)
+      console.log(colorsArray)
+    })
+}
