c6bb27b6abbf17ced92ec5dab2bf6615f53e39b7
Bastien Winant <bastien.winant@gmail.com>
2024-08-19 22:03:58 +0200

    webpack config with template html

---
+++ module-08/color-scheme-generator/public/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello World')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSwwQiIsInNvdXJjZXMiOlsid2VicGFjazovL2NvbG9yLXNjaGVtZS1nZW5lcmF0b3IvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hlbGxvIFdvcmxkJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
