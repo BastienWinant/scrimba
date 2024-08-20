b28d7e60a29ebe22da64b08fb545f5d8dcf85321
Bastien Winant <bastien.winant@gmail.com>
2024-08-20 21:29:34 +0200

    implement auth functionalities

--- module-08/color-scheme-generator/src/firebase-app.js
+++ module-08/color-scheme-generator/src/firebase-app.js
@@ -2,12 +2,27 @@ import { initializeApp } from "firebase/app"
 import {
   getAuth,
   connectAuthEmulator,
-  signInWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
 } from "firebase/auth"
 
+const loginModal = document.querySelector('#login-modal')
 const loginEmailInput = document.querySelector('#login-email')
 const loginPasswordInput = document.querySelector('#login-password')
 const loginBtn = document.querySelector('#login-btn')
+const cancelLoginBtn = document.querySelector('#cancel-login-btn')
+
+const signupModal = document.querySelector('#signup-modal')
+const signupEmailInput = document.querySelector('#signup-email')
+const signupPasswordInput = document.querySelector('#signup-password')
+const signupBtn = document.querySelector('#signup-btn')
+const cancelSignupBtn = document.querySelector('#cancel-signup-btn')
+
+
+const headerLoginBtn = document.querySelector('#header-login-btn')
+const headerSignupBtn = document.querySelector('#header-signup-btn')
+const headerLogoutBtn = document.querySelector('#header-logout-btn')
 
 // Firebase configuration
 const firebaseConfig = {
@@ -28,14 +43,67 @@ if (process.env.NODE_ENV == 'development') {
   connectAuthEmulator(firebaseAuth, "http://localhost:9099")
 }
 
+async function signupEmailPassword() {
+  const signupEmail = signupEmailInput.value
+  const signupPassword = signupPasswordInput.value
+
+  createUserWithEmailAndPassword(firebaseAuth, signupEmail, signupPassword)
+    .then(userCredential => {
+      console.log(userCredential)
+    })
+    .catch(error => {
+      const errorCode = error.code
+      const errorMessage = error.message
+
+      console.log(`${errorCode}: ${errorMessage}`)
+    })
+}
+
+signupBtn.addEventListener('click', e => {
+  e.preventDefault()
+  signupEmailPassword()
+  signupModal.close()
+})
+
+
 async function loginEmailPassword() {
   const loginEmail = loginEmailInput.value
   const loginPassword = loginPasswordInput.value
 
   signInWithEmailAndPassword(firebaseAuth, loginEmail, loginPassword)
+    .then(userCredential => {
+      console.log(userCredential)
+    })
+    .catch(error => {
+      const errorCode = error.code
+      const errorMessage = error.message
+
+      console.log(`${errorCode}: ${errorMessage}`)
+    })
 }
 
 loginBtn.addEventListener('click', e => {
-  e.preventDefault();
-  console.log('Hello World')
+  e.preventDefault()
+  loginEmailPassword()
+  loginModal.close()
+})
+
+headerLogoutBtn.addEventListener('click', e => {
+  signOut(firebaseAuth)
+})
+
+headerLoginBtn.addEventListener('click', e => {
+  loginModal.showModal()
+})
+
+headerSignupBtn.addEventListener('click', e => {
+  signupModal.showModal()
+})
+
+cancelLoginBtn.addEventListener('click', () => {
+  loginModal.close()
+})
+
+cancelSignupBtn.addEventListener('click', () => {
+  signupModal.close()
 })
--- module-08/color-scheme-generator/src/index.js
+++ module-08/color-scheme-generator/src/index.js
@@ -1,7 +1,21 @@
 import './style.css'
 
+import { onAuthStateChanged } from 'firebase/auth'
 import { firebaseAuth } from './firebase-app'
 
-const loginModal = document.querySelector('#login-modal')
+// const loginModal = document.querySelector('#login-modal')
 
-loginModal.showModal()
+// loginModal.showModal()
+
+async function monitorAuthChanges() {
+  onAuthStateChanged(firebaseAuth, user => {
+    console.log("Auth status changed")
+    if (user) {
+      console.log("User logged in")
+    } else {
+      console.log("User logged out")
+    }
+  })
+}
+
+monitorAuthChanges()
