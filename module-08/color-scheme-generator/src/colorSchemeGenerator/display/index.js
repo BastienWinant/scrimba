3dc65290bb7d55c96a0ed4f7d4e708a285dd6443
Bastien Winant <bastien.winant@gmail.com>
2024-08-21 12:16:20 +0200

    responsive nav

--- module-08/color-scheme-generator/src/colorSchemeGenerator/display/index.js
+++ module-08/color-scheme-generator/src/colorSchemeGenerator/display/index.js
@@ -4,6 +4,22 @@ const colorInput = document.querySelector('#color')
 const modeInput = document.querySelector('#mode')
 const submitBtn = document.querySelector('#generator-form-btn')
 
+function generateColorLis(colorsArr) {
+  return colorsArr.map(colorObj => {
+    const liEl = document.createElement('li')
+    liEl.classList.add('generator-color')
+    liEl.style.backgroundColor = colorObj.hex.value
+    return liEl
+  })
+}
+
+function renderGeneratorScheme(colorsArr) {
+  const colorsList = document.querySelector('#generator-colors')
+  colorsList.innerHTML = ''
+  const colorLiEls = generateColorLis(colorsArr)
+  colorsList.append(...colorLiEls)  
+}
+
 function updateGeneratorDisplay() {
   const colorHex = localStorage.getItem('gcs-color-hex')
   const colorMode = localStorage.getItem('gcs-color-mode')
@@ -15,12 +31,19 @@ function updateGeneratorDisplay() {
   fetch(requestUrl)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      renderGeneratorScheme(data.colors)
     })
 }
 
 submitBtn.addEventListener('click', e => {
   e.preventDefault()
+
+  const colorHex = colorInput.value.slice(1,)
+  localStorage.setItem('gcs-color-hex', colorHex)
+
+  const colorMode = modeInput.value
+  localStorage.setItem('gcs-color-mode', colorMode)
+
   updateGeneratorDisplay()
   console.log('Getting color')
 })
