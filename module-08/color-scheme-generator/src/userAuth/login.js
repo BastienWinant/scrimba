b2d9f96e08056c25cee5485b4a4048701844b296
Bastien Winant <bastien.winant@gmail.com>
2024-09-03 09:38:30 +0200

    Delete module-08/color-scheme-generator/src directory

--- module-08/color-scheme-generator/src/userAuth/login.js
+++
@@ -1,8 +0,0 @@
-
-import './style.css'
-
-export const loginModal = document.querySelector('#login-modal')
-export const loginEmailInput = document.querySelector('#login-email')
-export const loginPasswordInput = document.querySelector('#login-password')
-export const loginBtn = document.querySelector('#login-btn')
-export const cancelLoginBtn = document.querySelector('#cancel-login-btn')
--- module-08/color-scheme-generator/src/userAuth/signup.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-
-export const signupModal = document.querySelector('#signup-modal')
-export const signupEmailInput = document.querySelector('#signup-email')
-export const signupPasswordInput = document.querySelector('#signup-password')
-export const signupBtn = document.querySelector('#signup-btn')
-export const cancelSignupBtn = document.querySelector('#cancel-signup-btn')
