0ccdedde5023b2872cd9fe1c01f4e449a3e057cb
Bastien Winant <bastien.winant@gmail.com>
2024-09-11 18:35:28 +0200

    create dedicated container for data

--- module-08/movie-watchlist/index.js
+++ module-08/movie-watchlist/index.js
@@ -1,33 +1,6 @@
+const searchForm = document.querySelector('#search-form')
 const searchTermInput = document.querySelector('#search-term')
-const resultTypeInput = document.querySelector('#result-type')
-const searchSubmitBtn = document.querySelector('#search-submit-btn')
-
-async function omdbApiRequest(term, type) {
-  const baseUrl = 'http://www.omdbapi.com'
-  const apiKey = 'dc03cb5c'
-
-  const requestUrl = `${baseUrl}/?apikey=${apiKey}&s=${term}`
-  requestUrl += type ? `&type=${type}` : ''
-
-  const response = await fetch(requestUrl)
-  const data = await response.json()
-  
-  return data
-}
-async function displaySearchResults(e) {
-  e.preventDefault()
-
-  const searchTerm = searchTermInput.value
-  const resultType = resultTypeInput.value
-
-  let searchData
-
-  if (!searchTerm) {
-    // TODO: show error
-  } else {
-    searchData = await omdbApiRequest(searchTerm, resultType)
-  }
-
-  console.log(searchData)
-}
-searchSubmitBtn.addEventListener('click', displaySearchResults)
+const searchTypeInput = document.querySelector('#search-type')
+const searchYearInput = document.querySelector('#search-year')
+const searchSubmitBtn = document.querySelector('#submit-btn')
+const dataContainer = document.querySelector('#data-container')
