78a7a80c5adc5bfe3e2782dfe7f95a14b52f3f7f
Bastien Winant <bastien.winant@gmail.com>
2024-09-12 17:49:48 +0200

    display search results

--- module-08/movie-watchlist/index.js
+++ module-08/movie-watchlist/index.js
@@ -4,3 +4,449 @@ const searchTypeInput = document.querySelector('#search-type')
 const searchYearInput = document.querySelector('#search-year')
 const searchSubmitBtn = document.querySelector('#submit-btn')
 const dataContainer = document.querySelector('#data-container')
+
+function filterResults(title, type, year) {}
+
+
+async function omdbApiRequest(params) {
+  const apiKey = 'dc03cb5c'
+  const baseUrl = 'http://www.omdbapi.com'
+
+  let requestUrl = `${baseUrl}/?apikey=${apiKey}`
+
+  for (const key in params) {
+    if (params[key]) {
+      requestUrl += `&${key}=${params[key]}`
+    }
+  }
+  
+  const response = await fetch(requestUrl)
+
+  try {
+    const data = await response.json()
+    return data.Response === 'True' ? data : undefined
+  } catch {
+    return
+  }
+}
+
+async function fetchMovieDetails(movieId) {
+  const requestParams = {'i': movieId }
+  const data = await omdbApiRequest(requestParams)
+
+  return data
+}
+
+async function fetchMovieIDs(term, type, year, page) {
+  const requestParams = {
+    's': term,
+    'type': type,
+    'y': year,
+    'page': page
+  }
+
+  const data = await omdbApiRequest(requestParams)
+  const movieIds = data.Search.map(movie => movie.imdbID)
+  
+  return movieIds
+}
+
+async function fetchSearchResults(searchTerm, searchType, searchYear) {
+  const movieIds = await fetchMovieIDs(searchTerm, searchType, searchYear)
+  const movieDetails = await Promise.all(movieIds.map(id => fetchMovieDetails(id)))
+
+  return movieDetails
+}
+
+function generateResultHTML(resultsArr) {
+  const html = resultsArr.map(movieObj => {
+    const moviePoster = movieObj.Poster === 'N/A'
+          ? 'https://plus.unsplash.com/premium_photo-1684923604860-64e661f2ff72'
+          : movieObj.Poster
+    
+    let moviePlot = movieObj.Plot === 'N/A' ? '' : movieObj.Plot
+
+    return `<article class="card">
+      <img src="${moviePoster}" alt="movie poster" class="card-img">
+      <div class="card-body">
+        <header class="card-header">
+          <h3 class="movie-title">${movieObj.Title}</h3>
+          <div class="movie-rating">
+            <i class="fa-solid fa-star"></i>
+            ${movieObj.imdbRating}
+          </div>
+        </header>
+        <section class="movie-info">
+          <div class="movie-metadata">
+            <p>${movieObj.Runtime}</p>
+            <p>${movieObj.Genre}</p>
+          </div>
+          <button class="add-movie-btn" data-movie-id="${movieObj.imdbID}">
+            <i class="fa-solid fa-circle-plus fa-xl"></i>
+            Watchlist
+          </button>
+        </section>
+        <p class="movie-plot">${moviePlot}</p>
+      </div>
+    </article>
+    <hr class="card-separator">`
+  }).join('\n')
+
+  return html
+}
+
+async function displaySearchResults(e) {
+  e.preventDefault()
+
+  const searchTerm = searchTermInput.value
+  const searchType = searchTypeInput.value
+  const searchYear = searchYearInput.value
+
+  let searchResults
+  let searchResultHTML
+  if (searchTerm) {
+    // searchResults = await fetchSearchResults(searchTerm, searchType, searchYear)
+    searchResults = testData
+    searchResultHTML = generateResultHTML(searchResults)
+  } else {
+    searchResultHTML =
+      `<div class="container no-data-state">
+        <p>Unable to find what you’re looking for. Please try another search.</p>
+      </div>`
+  }
+
+  dataContainer.innerHTML = searchResultHTML
+}
+searchSubmitBtn.addEventListener('click', displaySearchResults)
+
+const testData = [
+  {
+      "Title": "The Equalizer",
+      "Year": "2014",
+      "Rated": "R",
+      "Released": "26 Sep 2014",
+      "Runtime": "132 min",
+      "Genre": "Action, Crime, Thriller",
+      "Director": "Antoine Fuqua",
+      "Writer": "Richard Wenk, Michael Sloan, Richard Lindheim",
+      "Actors": "Denzel Washington, Marton Csokas, Chloë Grace Moretz",
+      "Plot": "A man who believes he has put his mysterious past behind him cannot stand idly by when he meets a young girl under the control of ultra-violent Russian gangsters.",
+      "Language": "English, Russian, Spanish, German, Ukrainian",
+      "Country": "United States",
+      "Awards": "1 win & 9 nominations",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTQ2MzE2NTk0NF5BMl5BanBnXkFtZTgwOTM3NTk1MjE@._V1_SX300.jpg",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "7.2/10"
+          },
+          {
+              "Source": "Rotten Tomatoes",
+              "Value": "61%"
+          },
+          {
+              "Source": "Metacritic",
+              "Value": "57/100"
+          }
+      ],
+      "Metascore": "57",
+      "imdbRating": "7.2",
+      "imdbVotes": "429,079",
+      "imdbID": "tt0455944",
+      "Type": "movie",
+      "DVD": "N/A",
+      "BoxOffice": "$101,530,738",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer 2",
+      "Year": "2018",
+      "Rated": "R",
+      "Released": "20 Jul 2018",
+      "Runtime": "121 min",
+      "Genre": "Action, Crime, Thriller",
+      "Director": "Antoine Fuqua",
+      "Writer": "Richard Wenk, Michael Sloan, Richard Lindheim",
+      "Actors": "Denzel Washington, Pedro Pascal, Ashton Sanders",
+      "Plot": "Robert McCall serves an unflinching justice for the exploited and oppressed, but how far will he go when that is someone he loves?",
+      "Language": "English, French, Turkish, Hebrew, Arabic, Spanish",
+      "Country": "United States",
+      "Awards": "1 win & 4 nominations",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTU2OTYzODQyMF5BMl5BanBnXkFtZTgwNjU3Njk5NTM@._V1_SX300.jpg",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "6.7/10"
+          },
+          {
+              "Source": "Rotten Tomatoes",
+              "Value": "52%"
+          },
+          {
+              "Source": "Metacritic",
+              "Value": "50/100"
+          }
+      ],
+      "Metascore": "50",
+      "imdbRating": "6.7",
+      "imdbVotes": "203,772",
+      "imdbID": "tt3766354",
+      "Type": "movie",
+      "DVD": "N/A",
+      "BoxOffice": "$102,084,362",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer 3",
+      "Year": "2023",
+      "Rated": "R",
+      "Released": "01 Sep 2023",
+      "Runtime": "109 min",
+      "Genre": "Action, Crime, Thriller",
+      "Director": "Antoine Fuqua",
+      "Writer": "Richard Wenk, Michael Sloan, Richard Lindheim",
+      "Actors": "Denzel Washington, Dakota Fanning, Eugenio Mastrandrea",
+      "Plot": "Robert McCall finds himself at home in Southern Italy but he discovers his friends are under the control of local crime bosses. As events turn deadly, McCall knows what he has to do: become his friends' protector by taking on the ...",
+      "Language": "English, Italian",
+      "Country": "United States, Italy",
+      "Awards": "6 nominations",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYjZjYTllOWItYjUxMC00NTA0LWI1ZjItNjE5YjVhZjRmYmYwXkEyXkFqcGdeQXVyMTcwOTQzOTYy._V1_SX300.jpg",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "6.8/10"
+          },
+          {
+              "Source": "Rotten Tomatoes",
+              "Value": "76%"
+          }
+      ],
+      "Metascore": "N/A",
+      "imdbRating": "6.8",
+      "imdbVotes": "127,721",
+      "imdbID": "tt17024450",
+      "Type": "movie",
+      "DVD": "N/A",
+      "BoxOffice": "$92,373,751",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer",
+      "Year": "2021–",
+      "Rated": "TV-14",
+      "Released": "07 Feb 2021",
+      "Runtime": "N/A",
+      "Genre": "Action, Crime, Drama",
+      "Director": "N/A",
+      "Writer": "Andrew W. Marlowe, Terri Edda Miller",
+      "Actors": "Queen Latifah, Tory Kittles, Adam Goldberg",
+      "Plot": "An enigmatic figure uses her extensive skills to help those with nowhere else to turn.",
+      "Language": "English",
+      "Country": "United States",
+      "Awards": "2 wins & 16 nominations",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYmY3MTgwYWUtMTc2NC00ZDdmLWIyNDAtOGJkNTQyZjM0NDA5XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "5.5/10"
+          }
+      ],
+      "Metascore": "N/A",
+      "imdbRating": "5.5",
+      "imdbVotes": "17,365",
+      "imdbID": "tt11242246",
+      "Type": "series",
+      "totalSeasons": "4",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer",
+      "Year": "1985–1989",
+      "Rated": "TV-PG",
+      "Released": "18 Sep 1985",
+      "Runtime": "48 min",
+      "Genre": "Action, Crime, Drama",
+      "Director": "N/A",
+      "Writer": "Richard Lindheim, Michael Sloan",
+      "Actors": "Edward Woodward, Keith Szarabajka, Robert Lansing",
+      "Plot": "A retired Intelligence Agent turned private detective helps various threatened clients to equalize the odds.",
+      "Language": "English",
+      "Country": "United States",
+      "Awards": "Nominated for 7 Primetime Emmys. 2 wins & 11 nominations total",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjAzMzAyODMxMF5BMl5BanBnXkFtZTcwNjcyMjE2MQ@@._V1_SX300.jpg",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "7.8/10"
+          }
+      ],
+      "Metascore": "N/A",
+      "imdbRating": "7.8",
+      "imdbVotes": "6,031",
+      "imdbID": "tt0088513",
+      "Type": "series",
+      "totalSeasons": "4",
+      "Response": "True"
+  },
+  {
+      "Title": "Equalizer 2000",
+      "Year": "1987",
+      "Rated": "R",
+      "Released": "04 Jan 1988",
+      "Runtime": "85 min",
+      "Genre": "Action, Adventure, Sci-Fi",
+      "Director": "Cirio H. Santiago",
+      "Writer": "Joe Mari Avellana, Frederick Bailey",
+      "Actors": "Richard Norton, Corinne Wahl, William Steis",
+      "Plot": "A ruthless vehicular gang rules the post-apocalyptic wasteland. That's until a muscled hero named Slade builds the ultimate machine gun - Equalizer 2000, and declares a one man war on the gang's \"piece of garbage\" leader.",
+      "Language": "English",
+      "Country": "United States, Philippines",
+      "Awards": "N/A",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzllZGY2MjAtMTZhNy00Mzk1LWJkNzQtNmJmZDA4OGQ1N2M3XkEyXkFqcGdeQXVyMTQ2MjQyNDc@._V1_SX300.jpg",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "4.1/10"
+          }
+      ],
+      "Metascore": "N/A",
+      "imdbRating": "4.1",
+      "imdbVotes": "715",
+      "imdbID": "tt0091012",
+      "Type": "movie",
+      "DVD": "21 Jun 2018",
+      "BoxOffice": "N/A",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer",
+      "Year": "2016",
+      "Rated": "N/A",
+      "Released": "21 Jul 2016",
+      "Runtime": "N/A",
+      "Genre": "Documentary, Sport",
+      "Director": "Rebecca Snow",
+      "Writer": "Allen Booth",
+      "Actors": "Maria Vasallo",
+      "Plot": "What if the greatest high-performance athletes - present and past - could compete against each other on a level playing field?",
+      "Language": "English, German",
+      "Country": "Canada",
+      "Awards": "N/A",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYzNkNmE1MGEtZjc2MS00ODlmLTkyNDUtYjBjYmRmOTQ0N2UwXkEyXkFqcGdeQXVyMjUwNDY0MzY@._V1_SX300.jpg",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "6.4/10"
+          }
+      ],
+      "Metascore": "N/A",
+      "imdbRating": "6.4",
+      "imdbVotes": "20",
+      "imdbID": "tt4648652",
+      "Type": "movie",
+      "DVD": "N/A",
+      "BoxOffice": "N/A",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer 2 Special",
+      "Year": "2018",
+      "Rated": "N/A",
+      "Released": "14 Aug 2018",
+      "Runtime": "60 min",
+      "Genre": "Documentary",
+      "Director": "Dan Storey",
+      "Writer": "Tim Postins, Dan Storey, Alex Zane",
+      "Actors": "Liz Bernstrom, Todd Black, Sean Devereaux",
+      "Plot": "N/A",
+      "Language": "English",
+      "Country": "United Kingdom",
+      "Awards": "N/A",
+      "Poster": "N/A",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "7.4/10"
+          }
+      ],
+      "Metascore": "N/A",
+      "imdbRating": "7.4",
+      "imdbVotes": "10",
+      "imdbID": "tt8814938",
+      "Type": "movie",
+      "DVD": "N/A",
+      "BoxOffice": "N/A",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer: Children of the Night",
+      "Year": "2014",
+      "Rated": "N/A",
+      "Released": "30 Dec 2014",
+      "Runtime": "5 min",
+      "Genre": "Short",
+      "Director": "N/A",
+      "Writer": "N/A",
+      "Actors": "Todd Black, Jason Blumenthal, Antoine Fuqua, Chloë Grace Moretz",
+      "Plot": "This short features Blumenthal, Black, Fuqua, Moretz, Washington and Wenk. We learn about Moretz's role and performance as well as info about teen prostitutes.",
+      "Language": "English",
+      "Country": "USA",
+      "Awards": "N/A",
+      "Poster": "N/A",
+      "Ratings": [
+          {
+              "Source": "Internet Movie Database",
+              "Value": "7.6/10"
+          }
+      ],
+      "Metascore": "N/A",
+      "imdbRating": "7.6",
+      "imdbVotes": "5",
+      "imdbID": "tt5258176",
+      "Type": "movie",
+      "DVD": "N/A",
+      "BoxOffice": "N/A",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  },
+  {
+      "Title": "The Equalizer: One Man Army - Training and Fighting",
+      "Year": "2014",
+      "Rated": "N/A",
+      "Released": "30 Dec 2014",
+      "Runtime": "7 min",
+      "Genre": "Short",
+      "Director": "N/A",
+      "Writer": "N/A",
+      "Actors": "Jason Blumenthal, Antoine Fuqua, Denzel Washington, Richard Wenk",
+      "Plot": "This short offers material from Blumenthal, Fuqua, Washington, Wenk, and stunt coordinator Keith Woulard. \"Army\" tells us about the movie's action and fight material.",
+      "Language": "English",
+      "Country": "USA",
+      "Awards": "N/A",
+      "Poster": "N/A",
+      "Ratings": [],
+      "Metascore": "N/A",
+      "imdbRating": "N/A",
+      "imdbVotes": "N/A",
+      "imdbID": "tt5258178",
+      "Type": "movie",
+      "DVD": "N/A",
+      "BoxOffice": "N/A",
+      "Production": "N/A",
+      "Website": "N/A",
+      "Response": "True"
+  }
+]
