3910a0113c8d011eee7fc18bbf4b8a1586497c71
Bastien Winant <bastien.winant@gmail.com>
2024-10-24 00:02:51 +0200

    import trips data

--- module-10/travel-journal/src/App.js
+++ module-10/travel-journal/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Header from './components/header/Header'
 
+import tripsData from './assets/data/trips.json'
+
 function App() {
+  console.log(tripsData)
   return (
     <>
       <div className='app-container'>
