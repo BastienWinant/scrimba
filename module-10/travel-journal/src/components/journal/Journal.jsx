605a9591f9ab9f92da4c8417f5f37a297313df8b
Bastien Winant <bastien.winant@gmail.com>
2024-10-26 23:44:00 +0200

    add a state to the Journal component

--- module-10/travel-journal/src/components/journal/Journal.jsx
+++ module-10/travel-journal/src/components/journal/Journal.jsx
@@ -1,9 +1,17 @@
 import './Journal.css'
 
+import { useState } from 'react'
+
 import Feed from './feed/Feed'
 import Display from './display/Display'
 
 export default function Journal(props) {
+  const [cardIndex, setCardIndex] = useState(0)
+
+  const updateCardIndex = (idx) => {
+    setCardIndex(idx)
+  }
+
   return (
     <div className="journal">
       <Feed entries={props.entries} />
