f604116cb310aeffc7f9319bfccfdb04a2866bdc
Bastien Winant <bastien.winant@gmail.com>
2024-10-27 18:22:18 +0100

    add google maps link to display

--- module-10/travel-journal/src/components/journal/display/Display.jsx
+++ module-10/travel-journal/src/components/journal/display/Display.jsx
@@ -6,10 +6,13 @@ export default function Display(props) {
     <section className="display">
       <header className="display-header">
         <h2 className="display-title">{props.tripObj.title}</h2>
-        <div className="display-location">
-          <img src={PinIcon} alt="Pin icon." className="display-pin-icon" />
-          <p className="display-location-name">{props.tripObj.location}</p>
-        </div>
+        <section className='display-subtitle'>
+          <div className="display-location">
+            <img src={PinIcon} alt="Pin icon." className="display-pin-icon" />
+            <p className="display-location-name">{props.tripObj.location}</p>
+          </div>
+          <a href={props.tripObj.googleMapsUrl} className='display-link' target='_blank'>View on Google Maps</a>
+        </section>
       </header>
       <img src={props.tripObj.imageUrl} alt="Trip photo." className="display-img" />
       <p className="display-description">{props.tripObj.description}</p>
