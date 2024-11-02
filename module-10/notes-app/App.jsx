a098daefdfdba4ad06a42158258411752be1ab36
Bastien Winant <bastien.winant@gmail.com>
2024-11-02 22:18:19 +0100

    keep track of note create/update time

--- module-10/notes-app/App.jsx
+++ module-10/notes-app/App.jsx
@@ -3,14 +3,12 @@ import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import Split from "react-split"
 import { nanoid } from "nanoid"
-import { onSnapshot, addDoc, deleteDoc, doc } from "firebase/firestore"
+import { onSnapshot, addDoc, deleteDoc, doc, setDoc } from "firebase/firestore"
 import { db, notesCollection } from "./firebase"
 
 export default function App() {
 	const [notes, setNotes] = React.useState([])
-	const [currentNoteId, setCurrentNoteId] = React.useState(
-		(notes[0]?.id) || ""
-	)
+	const [currentNoteId, setCurrentNoteId] = React.useState("")
 	
 	const currentNote = 
 		notes.find(note => note.id === currentNoteId) 
@@ -29,28 +27,25 @@ export default function App() {
 		return unsubscribe
 	}, [])
 
+	React.useEffect(() => {
+		if (!currentNoteId) {
+			setCurrentNoteId(notes[0]?.id)
+		}
+	}, [notes])
+
 	async function createNewNote() {
 		const newNote = {
-			body: "# Type your markdown note's title here"
+			body: "# Type your markdown note's title here",
+			createdAt: Date.now(),
+			updatedAt: Date.now()
 		}
 		const newNoteRef = await addDoc(notesCollection, newNote)
 		setCurrentNoteId(newNoteRef.id)
 	}
 
-	function updateNote(text) {
-		setNotes(oldNotes => {
-			const newArray = []
-			for (let i = 0; i < oldNotes.length; i++) {
-				const oldNote = oldNotes[i]
-				if (oldNote.id === currentNoteId) {
-					// Put the most recently-modified note at the top
-					newArray.unshift({ ...oldNote, body: text })
-				} else {
-					newArray.push(oldNote)
-				}
-			}
-			return newArray
-		})
+	async function updateNote(text) {
+		const docRef = doc(db, "notes", currentNoteId)
+		await setDoc(docRef, { body: text, updatedAt: Date.now() }, { merge: true })
 	}
 
 	async function deleteNote(noteId) {
@@ -75,14 +70,10 @@ export default function App() {
 								newNote={createNewNote}
 								deleteNote={deleteNote}
 							/>
-							{
-								currentNoteId &&
-								notes.length > 0 &&
-								<Editor
-									currentNote={currentNote}
-									updateNote={updateNote}
-								/>
-							}
+							<Editor
+								currentNote={currentNote}
+								updateNote={updateNote}
+							/>
 						</Split>
 						:
 						<div className="no-notes">
