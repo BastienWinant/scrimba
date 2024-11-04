9fb4d62e7c0db1fb1708560fb706101949eeab50
Bastien Winant <bastien.winant@gmail.com>
2024-11-04 15:03:28 +0100

    create and render Die components

--- module-10/tenzies-game/src/App.jsx
+++ module-10/tenzies-game/src/App.jsx
@@ -1,8 +1,46 @@
+import React from 'react'
 import './App.css'
+import Die from './Die'
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    let newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      })
+    }
+
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  function holdDice(id) {
+    console.log(id)
+  }
+
+  const dieElements = dice.map(die => <Die
+                                        key={die.id}
+                                        value={die.value}
+                                        isHeld={die.isHeld}
+                                        holdDice={() => holdDice(die.id)}
+                                      />)
+
   return (
-    <></>
+    <main>
+      <div className='dice-container'>
+        {dieElements}
+      </div>
+      <button className="roll-dice" type="button" onClick={rollDice}>Roll</button>
+    </main>
   )
 }
 
