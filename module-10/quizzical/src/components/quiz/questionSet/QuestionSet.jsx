30b5d6ce3b3baff5f23baeb6f566b3fb18053b33
Bastien Winant <bastien.winant@gmail.com>
2024-11-06 21:54:45 +0100

    add visual cues for correct/incorrect answers

--- module-10/quizzical/src/components/quiz/questionSet/QuestionSet.jsx
+++ module-10/quizzical/src/components/quiz/questionSet/QuestionSet.jsx
@@ -5,20 +5,43 @@ import { nanoid } from 'nanoid'
 export default function QuestionSet(props) {
   const answerOptions = props.question.options.map(option => {
     const answerId = nanoid()
-    
+    const checked = option === props.formData[props.question.id]
+    const correctAnswer = props.question.correct_answer === option
+
+    let style = {}
+    if (props.disabled) {// if the quiz has been submitted
+      console.log(correctAnswer)
+      if (correctAnswer) {
+        style = {
+          backgroundColor: '#94D7A2',
+          border: '1px solid #94D7A2'
+        }
+      } else if (checked) {
+        style = {
+          backgroundColor: '#F8BCBC',
+          border: '1px solid #F8BCBC',
+          opacity: '.5'
+        }
+      } else {
+        style = {
+          opacity: '.5'
+        }
+      }
+    }
     return <label htmlFor={answerId} key={answerId}>
             <input
               type="radio"
               id={answerId}
               name={props.question.id}
               value={option}
-              checked={option === props.formData[props.question.id]}
+              checked={checked}
               onChange={props.handleChange}
               disabled={props.disabled}
             />
-            <span>{option}</span>
+            <span style={style}>{option}</span>
           </label>
   })
+
   return (
     <fieldset className="question-set">
       <legend className="question">{props.question.question}</legend>
